147bccbf8c31e9c0aaafd4e5e5844fbfa70840eb
Agostino Cavarero <agostino.cavarero@gmail.com>
2024-05-04 19:37:34 +0200

    use es6 modules

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const path = require("path");
+import express from 'express';
 
 const port = 3000;
 
 const app = express();
 app.set('trust proxy', true);
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, '../templates'));
-app.use(express.static(path.join(__dirname, '../assets')));
+app.set("views", 'templates');
+app.use(express.static('assets'));
 
 app.get('/', async (req, res) => {
     let clockOffset = 0;
